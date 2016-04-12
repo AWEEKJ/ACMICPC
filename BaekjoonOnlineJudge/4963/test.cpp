00457d23f085d6b752c55ef13ebe3e47d355bc79
Hanju Jo <gkswn121@gmail.com>
2016-04-13 04:57:25 +0900

    commit

--- BaekjoonOnlineJudge/4963/test.cpp
+++ BaekjoonOnlineJudge/4963/test.cpp
@@ -1,61 +1,48 @@
 #include <cstdio>
-#include <vector>
+#include <cstring>
 using namespace std;
 
 int w, h;
-vector<vector<int> > map;
+int map[55][55];
+int visited[55][55];
 int dir[2][8] = {
   {0, 1, 1, 1, 0, -1, -1, -1}, {1, 1, 0, -1, -1, -1, 0, 1}
 };
-
-void print_maze(){
-	printf("\n");
-	for(int i = 0; i < h; i++){
-		for(int j = 0; j < w; j++){
-			printf("%d ", map[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-void f(int x, int y, int num) {
-  print_maze();
-  int nx, ny;
+int piece;
+void dfs (int i, int j) {
+  if (visited[i][j] || !map[i][j]) return;
+  visited[i][j] = 1;
   for (int k = 0; k < 8; k++) {
-    nx = x + dir[0][k];
-    ny = y + dir[1][k];
-    if (map[nx][ny] == 1) {
-      map[nx][ny] = num;
-      printf("add %d %d\n", nx, ny);
-      f(nx, ny, num);
-      map[nx][ny] = 0;
-    }
+    dfs(i + dir[0][k], j + dir[1][k]);
   }
+  return;
 }
 
 int main() {
-  while(1){
-    scanf("%d%d", &w, &h);
-    if (w == 0 && h == 0) return 0;
+  while (scanf("%d%d", &w, &h)) {
+    if (w == 0 && h == 0) break;
 
-    map.resize(h, vector<int> (w, 0));
+    memset(map, 0, sizeof(map));
+    memset(visited, 0, sizeof(visited));
+
     for (int i = 0; i < h; i++) {
       for (int j = 0; j < w; j++) {
         scanf("%d", &map[i][j]);
       }
     }
 
-    int island = 1;
+    int piece = 0;
     for (int i = 0; i < h; i++) {
       for (int j = 0; j < w; j++) {
-        if (map[i][j] == 1) {
-          island++;
-          map[i][j] = island;
-          f(i, j, island);
+        if (visited[i][j] == 0 && map[i][j] == 1) {
+          piece++;
+          printf("i: %d, j: %d, p: %d\n", i, j, piece);
+          dfs(i, j);
         }
       }
     }
-    printf("%d\n", island - 1);
+
+    printf("%d\n", piece);
   }
   return 0;
 }
